8c253838cb22b513440fc4e48dd689604d03b63c
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-02-08 20:21:15 +0200

    Updated-2.

--- app/src/main/java/com/example/myapplist/MainActivity.java
+++ app/src/main/java/com/example/myapplist/MainActivity.java
@@ -1,21 +1,55 @@
 package com.example.myapplist;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-import com.example.myapplist.R;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
-    String[] fruits = {"Apple", "Orange", "Banana"};
+    Button BTN_ADD1;
+    TextView NUMBS_ADD1;
+    ListView LISTVIEW1;
+
+    ArrayList<CharSequence> numbsList = new ArrayList<CharSequence>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView fruitsList = (ListView) findViewById(R.id.list1);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, fruits);
-        fruitsList.setAdapter(adapter);
+        BTN_ADD1 = (Button) findViewById(R.id.btn_add1);
+        NUMBS_ADD1 = (TextView) findViewById(R.id.numb_add1);
+        LISTVIEW1 = (ListView) findViewById(R.id.list1);
+
+        final ListView NumbsList = (ListView) findViewById(R.id.list1);
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, numbsList);
+        NumbsList.setAdapter(adapter);
+
+        NumbsList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Toast.makeText(getApplicationContext(), numbsList.get(position), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        BTN_ADD1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                CharSequence a1 = NUMBS_ADD1.getText();
+                numbsList.add(a1);
+                LISTVIEW1.setAdapter(adapter);
+            }
+        });
     }
 }
