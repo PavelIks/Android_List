c8a147edc10b17f58644002cafc248a95dd93a1b
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-02-09 18:52:46 +0200

    Updated-3.

--- app/src/main/java/com/example/myapplist/MainActivity.java
+++ app/src/main/java/com/example/myapplist/MainActivity.java
@@ -1,23 +1,25 @@
 package com.example.myapplist;
 import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
     Button BTN_ADD1;
     TextView NUMBS_ADD1;
+    EditText WORDS_ADD1;
     ListView LISTVIEW1;
+    ArrayList<String> product_array_list = new ArrayList<String>();
 
-    ArrayList<CharSequence> numbsList = new ArrayList<CharSequence>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,30 +28,41 @@ public class MainActivity extends AppCompatActivity
 
         BTN_ADD1 = (Button) findViewById(R.id.btn_add1);
         NUMBS_ADD1 = (TextView) findViewById(R.id.numb_add1);
+        WORDS_ADD1 = (EditText) findViewById(R.id.string_add1);
         LISTVIEW1 = (ListView) findViewById(R.id.list1);
 
         final ListView NumbsList = (ListView) findViewById(R.id.list1);
-        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, numbsList);
-        NumbsList.setAdapter(adapter);
-
-        NumbsList.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                Toast.makeText(getApplicationContext(), numbsList.get(position), Toast.LENGTH_LONG).show();
-            }
-        });
+        ArrayAdapter<ArrayList> adapter = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, product_array_list);
 
         BTN_ADD1.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                CharSequence a1 = NUMBS_ADD1.getText();
-                numbsList.add(a1);
+                product_array_list.add((String.valueOf(((TextView) findViewById(R.id.numb_add1)).getText())));
+                product_array_list.add((String.valueOf(((TextView) findViewById(R.id.string_add1)).getText())));
                 LISTVIEW1.setAdapter(adapter);
             }
         });
+
+
+//        LISTVIEW1.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//        {
+//            @Override
+//            public void onItemClick(AdapterView<?> arg0, View v, int arg2, long arg3)
+//            {
+//                TextView tv = (TextView) v.findViewById(R.id.string_add1);
+//                Toast.makeText(getApplicationContext(), tv.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+//        LISTVIEW1.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//        {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+//            {
+//                Toast.makeText(getApplicationContext(), product_array_list.get(position), Toast.LENGTH_LONG).show();
+//            }
+//        });
     }
 }
